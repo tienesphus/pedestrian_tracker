11fd39897d434e6eb19493bd2f6c4d784076b55f
mdsimmo <mdsimmo2+git@gmail.com>
2019-06-14 16:47:32 +1000

    Better libbuscount unit test
    Removed dependency on detectors working (so it is a UNIT test)
    
    also made it run a few times

--- tests/test_libbuscount.cpp
+++ tests/test_libbuscount.cpp
@@ -1,27 +1,62 @@
 #include "catch.hpp"
 
 #include "libbuscount.hpp"
-#include "detector_opencv.hpp"
+#include "detector.hpp"
 #include "testing_utils.hpp"
+
+class DummyDetector: public Detector {
+    Detections process(const cv::Mat&) override {
+        std::vector<Detection> results;
+        results.emplace_back(cv::Rect(1, 2, 3, 4), 0.5);
+        return Detections(results);
+    }
+};
 
 TEST_CASE( "Bus Counter runs in serial", "[libbuscount]" ) {
 
-    NetConfig netConfig = load_test_config(cv::dnn::DNN_BACKEND_OPENCV, cv::dnn::DNN_TARGET_CPU);
-    OpenCVDetector detector(<#initializer#>, netConfig);
+    DummyDetector detector;
 
     WorldConfig config = WorldConfig::from_file(std::string(SOURCE_DIR)+"/config.csv");
     Tracker tracker(config);
 
+    int count = 50;
     BusCounter counter(detector, tracker, config,
             []() -> std::optional<cv::Mat> {
                 return load_test_image();
             },
             [](const cv::Mat&) {
             },
-            []() -> bool {
-                return true;
+            [&count]() -> bool {
+                --count;
+                return count < 0;
             }
     );
 
     counter.run(BusCounter::RUN_SERIAL, false);
+    // just get to the end without crashing
+}
+
+
+TEST_CASE( "Bus Counter runs in parallel", "[libbuscount]" ) {
+
+    DummyDetector detector;
+
+    WorldConfig config = WorldConfig::from_file(std::string(SOURCE_DIR)+"/config.csv");
+    Tracker tracker(config);
+
+    int count = 50;
+    BusCounter counter(detector, tracker, config,
+                       []() -> std::optional<cv::Mat> {
+                           return load_test_image();
+                       },
+                       [](const cv::Mat&) {
+                       },
+                       [&count]() -> bool {
+                           --count;
+                           return count < 0;
+                       }
+    );
+
+    counter.run(BusCounter::RUN_PARALLEL, false);
+    // just get to the end without crashing
 }
