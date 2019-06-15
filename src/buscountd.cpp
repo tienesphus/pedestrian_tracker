789e757ceb63445ea71b8a1f21f9b9ffbfe6ee2d
Alastair Knowles <2062674@student.swin.edu.au>
2019-06-15 21:05:57 +1000

    gst buscount plugin partially done. Builds as .so for easy debugging

--- src/buscountd.cpp
+++ src/buscountd.cpp
@@ -1,44 +1,42 @@
-#include <gst/gst.h>
+// C++ includes
+#include <gstreamermm.h>
+#include <glibmm/main.h>
+
+// C includes
 #include <gst/rtsp-server/rtsp-server.h>
 
+// Project includes
+#include "gstbuscountfilter.hpp"
 
-int main (int argc, char *argv[])
+GstRTSPMediaFactory *create_test_factory()
 {
+    // Make a media factory for a test stream. The default media factory can use
+    // gst-launch syntax to create pipelines. Any launch line works as long as the
+    // endpoints are named pay%d where %d is any number from 0 to INT_MAX. Each
+    // element named pay%d will be another stream for the given rtsp endpoint.
 
-    gst_init (&argc, &argv);
-
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
-
-    // Create a server instance
-    GstRTSPServer *server = gst_rtsp_server_new();
-
-    // get the mount points for this server, every server has a default object
-    // that be used to map uri mount points to media factories
-    GstRTSPMountPoints *mounts = gst_rtsp_server_get_mount_points(server);
-
-    // make a media factory for a test stream. The default media factory can use
-    // gst-launch syntax to create pipelines.
-    // any launch line works as long as it contains elements named pay%d. Each
-    // element with pay%d names will be a stream
+    // Test video
     GstRTSPMediaFactory *test_factory = gst_rtsp_media_factory_new();
     gst_rtsp_media_factory_set_launch(
         test_factory,
         "( "
-            "videotestsrc is-live=1 ! clockoverlay ! "
-            "omxh264enc ! video/x-h264, profile=baseline ! "
-            "rtph264pay name=pay0 pt=96 "
+            "videotestsrc is-live=1 ! buscountfilter ! clockoverlay ! videoconvert ! "
+            "video/x-raw, format=I420 ! omxh264enc ! "
+            "video/x-h264, profile=baseline ! rtph264pay name=pay0 pt=96 "
         ")"
     );
+
     // Set shared so that multiple devices can connect to the same stream
-    gst_rtsp_media_factory_set_shared (factory, TRUE);
+    gst_rtsp_media_factory_set_shared(test_factory, TRUE);
 
-    // attach the test factory to the /test url
-    gst_rtsp_mount_points_add_factory (mounts, "/test", test_factory);
- 
+    return test_factory;
+}
 
+GstRTSPMediaFactory *create_live_factory()
+{
     // Live video
     GstRTSPMediaFactory *live_factory = gst_rtsp_media_factory_new();
-    gst_rtsp_media_factory_set_launch (
+    gst_rtsp_media_factory_set_launch(
         live_factory,
         "( "
             "v4l2src ! clockoverlay ! "
@@ -46,20 +44,48 @@ int main (int argc, char *argv[])
             "rtph264pay name=pay0 pt=96 "
         ")"
     );
+
     // Set shared so that multiple devices can connect to the same stream
-    gst_rtsp_media_factory_set_shared (factory, TRUE);
-    // attach the test factory to the /live url
-    gst_rtsp_mount_points_add_factory (mounts, "/live", factory);
+    gst_rtsp_media_factory_set_shared(live_factory, TRUE);
 
-    // don't need the ref to the mapper anymore
-    g_object_unref (mounts);
+    return live_factory;
+}
 
-    // attach the server to the default maincontext
-    gst_rtsp_server_attach (server, NULL);
+int main (int argc, char *argv[])
+{
+    // Initialise gstreamermm
+    Gst::init(argc, argv);
 
-    // start serving
-    g_print ("stream ready at rtsp://127.0.0.1:8554/test\n");
-    g_main_loop_run (loop);
+    // Register the gstreamer buscount plugin
+    GstBusCount::plugin_init_static();
+
+    // Create a main loop for the current thread
+    Glib::RefPtr<Glib::MainLoop> loop = Glib::MainLoop::create();
+
+    // Create a new server instance
+    GstRTSPServer *server = gst_rtsp_server_new();
+
+    // Get a reference to the mount points object for this server. Every
+    // server has a default object that can be used to map URI mount points
+    // to media factories.
+    GstRTSPMountPoints *mounts = gst_rtsp_server_get_mount_points(server);
+
+    // Attach the test factory to the /test URI
+    gst_rtsp_mount_points_add_factory(mounts, "/test", create_test_factory());
+
+    // Attach the test factory to the /live URI
+    gst_rtsp_mount_points_add_factory(mounts, "/live", create_live_factory());
+
+    // Free the reference to the mount points object, not needed anymore
+    g_object_unref(mounts);
+
+    // Attach the server to the default MainLoop context
+    gst_rtsp_server_attach(server, NULL);
+
+    g_print("Streams ready at rtsp://<IP>:8554/test and rtsp://<IP>:8554/live\n");
+
+    // Start serving
+    loop->run();
 
     return 0;
 }
--- src/gstbuscountfilter.cpp
+++ src/gstbuscountfilter.cpp
@@ -1,290 +1,197 @@
-/*
- * GStreamer
- * Copyright (C) 2005 Thomas Vander Stichele <thomas@apestaart.org>
- * Copyright (C) 2005 Ronald S. Bultje <rbultje@ronald.bitfreak.net>
- * Copyright (C) 2019 Alastair Knowles <2062674@student.swin.edu.au>
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * Alternatively, the contents of this file may be used under the
- * GNU Lesser General Public License Version 2.1 (the "LGPL"), in
- * which case the following provisions apply instead of the ones
- * mentioned above:
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
 
-/**
- * SECTION:element-buscountfilter
- *
- * A gstreamer filter as a wrapper around buscount.
- *
- * <refsect2>
- * <title>Example launch line</title>
- * |[
- * gst-launch -v -m fakesrc ! buscountfilter ! fakesink silent=TRUE
- * ]|
- * </refsect2>
- */
+#include "gstbuscountfilter.hpp"
 
-#include <gst/gst.h>
-#include "gstbuscountfilter.h"
+#include <gstreamermm/wrap_init.h>
+#include <glib.h>
 
-// Default parameters and values for filter options. Many of these are
-// used for initialising default values, and may be overridable at
-// future points in time. Perhaps some of these should be moved to 
-// a separate config.h.in file???
-#define GST_CAT_DEFAULT gst_bus_count_filter_debug
+namespace GstBusCount {
 
-// A static struct for debugging purposes. If debugging is disabled during
-// build, this will expand to nothing.
-GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
+Gst::ValueList GstBusCountFilter::formats;
 
-// Filter signals and args
-enum
+static void value_list_string_init(Gst::ValueList &list, ...)
 {
-  /* FILL ME */
-  LAST_SIGNAL
-};
+    va_list args;
+    va_start(args, list);
 
-enum
-{
-  PROP_0,
-  PROP_SILENT
-};
-
-/* the capabilities of the inputs and outputs.
- *
- * describe the real formats here.
- */
-static GstStaticPadTemplate sink_factory = GST_STATIC_PAD_TEMPLATE ("sink",
-    GST_PAD_SINK,
-    GST_PAD_ALWAYS,
-    GST_STATIC_CAPS ("ANY")
-    );
-
-static GstStaticPadTemplate src_factory = GST_STATIC_PAD_TEMPLATE ("src",
-    GST_PAD_SRC,
-    GST_PAD_ALWAYS,
-    GST_STATIC_CAPS ("ANY")
-    );
-
-#define gst_bus_count_filter_parent_class parent_class
-G_DEFINE_TYPE (GstBusCountFilter, gst_bus_count_filter, GST_TYPE_ELEMENT);
-
-static void gst_bus_count_filter_set_property (GObject * object, guint prop_id,
-    const GValue * value, GParamSpec * pspec);
-static void gst_bus_count_filter_get_property (GObject * object, guint prop_id,
-    GValue * value, GParamSpec * pspec);
-
-static gboolean gst_bus_count_filter_sink_event (GstPad * pad, GstObject * parent, GstEvent * event);
-static GstFlowReturn gst_bus_count_filter_chain (GstPad * pad, GstObject * parent, GstBuffer * buf);
-
-/* GObject vmethod implementations */
-
-/* initialize the buscountfilter's class */
-static void
-gst_bus_count_filter_class_init (GstBusCountFilterClass * klass)
-{
-  GObjectClass *gobject_class;
-  GstElementClass *gstelement_class;
-
-  gobject_class = (GObjectClass *) klass;
-  gstelement_class = (GstElementClass *) klass;
-
-  gobject_class->set_property = gst_bus_count_filter_set_property;
-  gobject_class->get_property = gst_bus_count_filter_get_property;
-
-  g_object_class_install_property (gobject_class, PROP_SILENT,
-      g_param_spec_boolean ("silent", "Silent", "Produce verbose output ?",
-          FALSE, G_PARAM_READWRITE));
-
-  gst_element_class_set_details_simple(gstelement_class,
-    "BusCountFilter",
-    "FIXME:Generic",
-    "FIXME:Generic Template Element",
-    " <<user@hostname.org>>");
-
-  gst_element_class_add_pad_template (gstelement_class,
-      gst_static_pad_template_get (&src_factory));
-  gst_element_class_add_pad_template (gstelement_class,
-      gst_static_pad_template_get (&sink_factory));
-}
-
-/* initialize the new element
- * instantiate pads and add them to element
- * set pad calback functions
- * initialize instance structure
- */
-static void
-gst_bus_count_filter_init (GstBusCountFilter * filter)
-{
-  filter->sinkpad = gst_pad_new_from_static_template (&sink_factory, "sink");
-  gst_pad_set_event_function (filter->sinkpad,
-                              GST_DEBUG_FUNCPTR(gst_bus_count_filter_sink_event));
-  gst_pad_set_chain_function (filter->sinkpad,
-                              GST_DEBUG_FUNCPTR(gst_bus_count_filter_chain));
-  GST_PAD_SET_PROXY_CAPS (filter->sinkpad);
-  gst_element_add_pad (GST_ELEMENT (filter), filter->sinkpad);
-
-  filter->srcpad = gst_pad_new_from_static_template (&src_factory, "src");
-  GST_PAD_SET_PROXY_CAPS (filter->srcpad);
-  gst_element_add_pad (GST_ELEMENT (filter), filter->srcpad);
-
-  filter->silent = FALSE;
-}
-
-static void
-gst_bus_count_filter_set_property (GObject * object, guint prop_id,
-    const GValue * value, GParamSpec * pspec)
-{
-  GstBusCountFilter *filter = GST_BUSCOUNTFILTER (object);
-
-  switch (prop_id) {
-    case PROP_SILENT:
-      filter->silent = g_value_get_boolean (value);
-      break;
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-      break;
-  }
-}
-
-static void
-gst_bus_count_filter_get_property (GObject * object, guint prop_id,
-    GValue * value, GParamSpec * pspec)
-{
-  GstBusCountFilter *filter = GST_BUSCOUNTFILTER (object);
-
-  switch (prop_id) {
-    case PROP_SILENT:
-      g_value_set_boolean (value, filter->silent);
-      break;
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-      break;
-  }
-}
-
-/* GstElement vmethod implementations */
-
-/* this function handles sink events */
-static gboolean
-gst_bus_count_filter_sink_event (GstPad * pad, GstObject * parent, GstEvent * event)
-{
-  GstBusCountFilter *filter;
-  gboolean ret;
-
-  filter = GST_BUSCOUNTFILTER (parent);
-
-  GST_LOG_OBJECT (filter, "Received %s event: %" GST_PTR_FORMAT,
-      GST_EVENT_TYPE_NAME (event), event);
-
-  switch (GST_EVENT_TYPE (event)) {
-    case GST_EVENT_CAPS:
+    char *str;
+    while (str = va_arg(args, char*))
     {
-      GstCaps * caps;
+        Glib::Value<Glib::ustring> val;
+        Glib::ustring ustr(str);
 
-      gst_event_parse_caps (event, &caps);
-      /* do something with the caps */
+        val.init(val.value_type());
+        val.set(ustr);
 
-      /* and forward */
-      ret = gst_pad_event_default (pad, parent, event);
-      break;
+        list.append(val);
     }
-    default:
-      ret = gst_pad_event_default (pad, parent, event);
-      break;
-  }
-  return ret;
+
+    va_end(args);
 }
 
-/* chain function
- * this function does the actual processing
- */
-static GstFlowReturn
-gst_bus_count_filter_chain (GstPad * pad, GstObject * parent, GstBuffer * buf)
+void GstBusCountFilter::class_init(Gst::ElementClass<GstBusCountFilter> *klass)
 {
-  GstBusCountFilter *filter;
+    formats.init(formats.get_type());
+    value_list_string_init(formats,
+        "BGRx", "RGBx", "xRGB", "xBGR", "RGBA",
+        "BGRA", "ARGB", "RGB", "BGR", NULL
+    );
 
-  filter = GST_BUSCOUNTFILTER (parent);
+    klass->set_metadata(
+            "Bus Counter Filter",
+            "Processor/Video/",
+            "Processes incoming video from a camera and extract person entry count",
+            "Alastair Knowles <2062674@student.swin.edu.au>");
 
-  if (filter->silent == FALSE)
-    g_print ("I'm plugged, therefore I'm in.\n");
+    // Looks ugly, but we can't use template/variadic constructor because it implicitly copies
+    // everything, and there's a bug when copying Glib::Value objects (they aren't fully
+    // initialised).
+    auto caps_struct = Gst::Structure("video/x-raw");
 
-  /* just push out the incoming buffer without touching it */
-  return gst_pad_push (filter->srcpad, buf);
+    caps_struct.set_field("format", static_cast<Glib::ValueBase&>(formats));
+    caps_struct.set_field("width", Gst::Range<int>(1, G_MAXINT32));
+    caps_struct.set_field("height", Gst::Range<int>(1, G_MAXINT32));
+    caps_struct.set_field("framerate", Gst::Range<Gst::Fraction>(
+            Gst::Fraction(0, 1),
+            Gst::Fraction(G_MAXINT32, 1)
+    ));
+
+    klass->add_pad_template(
+        Gst::PadTemplate::create(
+            "video_sink",
+            Gst::PAD_SINK,
+            Gst::PAD_ALWAYS,
+            Gst::Caps::create(caps_struct)
+        )
+    );
+
+    klass->add_pad_template(
+        Gst::PadTemplate::create(
+            "video_src",
+            Gst::PAD_SRC,
+            Gst::PAD_ALWAYS,
+            Gst::Caps::create(caps_struct)
+        )
+    );
 }
 
-
-/* entry point to initialize the plug-in
- * initialize the plug-in itself
- * register the element factories and other features
- */
-static gboolean
-buscountfilter_init (GstPlugin * buscountfilter)
+bool GstBusCountFilter::register_buscount_filter(Glib::RefPtr<Gst::Plugin> plugin)
 {
-  /* debug category for fltering log messages
-   *
-   * exchange the string 'Template buscountfilter' with your description
-   */
-  GST_DEBUG_CATEGORY_INIT (gst_bus_count_filter_debug, "buscountfilter",
-      0, "Template buscountfilter");
-
-  return gst_element_register (buscountfilter, "buscountfilter", GST_RANK_NONE,
-      GST_TYPE_BUSCOUNTFILTER);
+    return Gst::ElementFactory::register_element(
+        plugin,
+        "buscountfilter",
+        10,
+        Gst::register_mm_type<GstBusCountFilter>("buscountfilter")
+    );
 }
 
-/* PACKAGE: this is usually set by autotools depending on some _INIT macro
- * in configure.ac and then written into and defined in config.h, but we can
- * just set it ourselves here in case someone doesn't use autotools to
- * compile this code. GST_PLUGIN_DEFINE needs PACKAGE to be defined.
- */
-#ifndef PACKAGE
-#define PACKAGE "myfirstbuscountfilter"
-#endif
+bool GstBusCountFilter::register_buscount_filter(GstPlugin *plugin)
+{
+    return gst_element_register(
+        plugin,
+        "buscountfilter",
+        10,
+        Gst::register_mm_type<GstBusCountFilter>("buscountfilter")
+    );
+}
 
-/* gstreamer looks for this structure to register buscountfilters
- *
- * exchange the string 'Template buscountfilter' with your buscountfilter description
- */
-GST_PLUGIN_DEFINE (
+
+
+GstBusCountFilter::GstBusCountFilter(GstElement *gobj):
+        Glib::ObjectBase(typeid(GstBusCountFilter)),
+        Gst::Element(gobj),
+        test_property(*this, "test_property", "default_val")
+{
+    add_pad(video_in = Gst::Pad::create(get_pad_template("video_sink"), "video_sink"));
+    add_pad(video_out = Gst::Pad::create(get_pad_template("video_src"), "video_src"));
+
+    GST_PAD_SET_PROXY_CAPS(video_in->gobj());
+    GST_PAD_SET_PROXY_CAPS(video_out->gobj());
+
+    video_in->set_chain_function(sigc::mem_fun(*this, &GstBusCountFilter::chain));
+    video_in->set_event_function(sigc::mem_fun(*this, &GstBusCountFilter::sink_event));
+}
+
+// Events
+bool GstBusCountFilter::sink_event(const Glib::RefPtr<Gst::Pad> &pad, Glib::RefPtr<Gst::Event> &event)
+{
+    bool result;
+
+    switch (event->get_event_type())
+    {
+        case GST_EVENT_CAPS:
+        {
+            // Sink pad caps have been negotiated. Copy caps to source pad.
+            auto caps_event = Glib::RefPtr<Gst::EventCaps>::cast_static(event);
+            auto out_caps = caps_event->parse_caps()->copy();
+
+            g_print("### Set caps to %s", out_caps->to_string().c_str());
+
+            result = gst_pad_set_caps(video_out->gobj(), out_caps->gobj());
+            result &= pad->event_default(Glib::RefPtr<Gst::Event>(event));
+
+            break;
+        }
+
+        default:
+            result = pad->event_default(Glib::RefPtr<Gst::Event>(event));
+            break;
+
+    }
+
+    return result;
+}
+
+// Flow control
+Gst::FlowReturn GstBusCountFilter::chain(const Glib::RefPtr<Gst::Pad> &pad, Glib::RefPtr<Gst::Buffer> &buf)
+{
+    buf = buf->create_writable();
+
+    Gst::MapInfo mapinfo;
+    buf->map(mapinfo, Gst::MAP_WRITE);
+
+    std::sort(mapinfo.get_data(), mapinfo.get_data() + mapinfo.get_size());
+    buf->unmap(mapinfo);
+
+    return video_out->push(std::move(buf));
+}
+
+static bool plugin_init(const Glib::RefPtr<Gst::Plugin> &plugin)
+{
+    return GstBusCountFilter::register_buscount_filter(plugin->gobj());
+}
+
+static gboolean plugin_init(GstPlugin* plugin)
+{
+    // Ensure Glib is a go
+    Glib::init();
+    return GstBusCountFilter::register_buscount_filter(plugin);
+}
+
+} // end namespace GstBusCount
+
+extern "C"
+const GstPluginDesc gst_plugin_desc = {
     GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    buscountfilter,
-    "Template buscountfilter",
-    buscountfilter_init,
-    VERSION,
-    "LGPL",
-    "GStreamer",
-    "http://gstreamer.net/"
-)
+    "gstbuscount", "Project specific plugin for defining new gstreamer elements",
+    GstBusCount::plugin_init,
+    "1.0", "Proprietary",
+    "gstbuscountfilter.cpp", "buscountd",
+    "https://swinburne.com.au",
+    __GST_PACKAGE_RELEASE_DATETIME,
+    GST_PADDING_INIT
+};
+
+namespace GstBusCount {
+void plugin_init_static(void)
+{
+    Gst::Plugin::register_static(
+        gst_plugin_desc.major_version, gst_plugin_desc.minor_version,
+        gst_plugin_desc.name, gst_plugin_desc.description,
+        sigc::ptr_fun<const Glib::RefPtr<Gst::Plugin>&, bool>(&plugin_init),
+        gst_plugin_desc.version, gst_plugin_desc.license,
+        gst_plugin_desc.source, gst_plugin_desc.package,
+        gst_plugin_desc.origin
+    );
+}
+}
