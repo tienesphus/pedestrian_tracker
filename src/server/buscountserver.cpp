663f4de7903f5deb49b496afe987081495ea5e38
mdsimmo <mdsimmo2+git@gmail.com>
2019-07-18 09:53:31 +1000

    Drogon server now works
    Data is still just stored locally and unconnected from opencv

--- src/server/buscountserver.cpp
+++ src/server/buscountserver.cpp
@@ -5,55 +5,13 @@
 
 #include "../optional.hpp"
 
-struct Device
-{
-    std::string name;
-    std::string id;
-    std::string ip;
-    std::string default_feed;
-
-    Device(std::string name, std::string id, std::string ip, std::string default_feed):
-        name(std::move(name)), id(std::move(id)), ip(std::move(ip)), default_feed(std::move(default_feed))
-    {}
-
-    Json::Value to_json() const
-    {
-        Json::Value json;
-        json["ip"] = ip;
-        json["id"] = id;
-        json["name"] = name;
-        json["default_feed"] = default_feed;
-        return json;
-    }
-
-    static nonstd::optional<Device> from_json(Json::Value json)
-    {
-
-        Json::Value ip = json["ip"];
-        Json::Value id = json["id"];
-        Json::Value name = json["name"];
-        Json::Value default_feed = json["default_feed"];
-
-        if (!ip.isString() || !id.isString() || !name.isString() || !default_feed.isString()) {
-            return nonstd::nullopt;
-        } else {
-            std::string s_id = id.asString();
-            std::string s_ip = ip.asString();
-            std::string s_name = name.asString();
-            std::string s_default_feed = default_feed.asString();
-
-            return Device(s_id, s_ip, s_name, s_default_feed);
-        }
-    }
-};
-
 struct Feed
 {
     std::string name;
     std::string location;
 
     Feed(std::string name, std::string location):
-        name(std::move(name)), location(std::move(location))
+            name(std::move(name)), location(std::move(location))
     {}
 
     Json::Value to_json() const
@@ -62,6 +20,63 @@ struct Feed
         json["name"] = name;
         json["location"] = location;
         return json;
+    }
+
+    static nonstd::optional<Feed> from_json(const Json::Value& data)
+    {
+        Json::Value name = data["name"];
+        Json::Value location = data["location"];
+
+        if (name.isString() && location.isString()) {
+            std::cout << "Feed name or location is not a string" << std::endl;
+            return Feed(name.asString(), location.asString());
+        } else {
+            return nonstd::nullopt;
+        }
+    }
+};
+
+struct Device
+{
+    std::string name;
+    std::string id;
+    trantor::InetAddress ip;
+    Feed default_feed;
+
+    Device(std::string name, std::string id, const trantor::InetAddress& ip, Feed default_feed):
+        name(std::move(name)), id(std::move(id)), ip(ip), default_feed(std::move(default_feed))
+    {}
+
+    Json::Value to_json() const
+    {
+        Json::Value json;
+        json["ip"] = ip.toIp();
+        json["id"] = id;
+        json["name"] = name;
+        json["default_feed"] = default_feed.to_json();
+        return json;
+    }
+
+    static nonstd::optional<Device> from_json(Json::Value json, trantor::InetAddress ip)
+    {
+
+        Json::Value id = json["id"];
+        Json::Value name = json["name"];
+        Json::Value default_feed = json["default_feed"];
+
+        if (!id.isString() || !name.isString()) {
+            std::cout << "Device id or name is not a string" << std::endl;
+            return nonstd::nullopt;
+        } else {
+            std::string s_id = id.asString();
+            std::string s_name = name.asString();
+            nonstd::optional<Feed> def_feed = Feed::from_json(default_feed);
+
+            if (def_feed)
+                return Device(s_name, s_id, ip, *def_feed);
+            else
+                return nonstd::nullopt;
+        }
     }
 };
 
@@ -98,7 +113,7 @@ struct Line
         Json::Value x2 = json["x2"];
         Json::Value y2 = json["y2"];
 
-        if (!x1.isDouble() || !y1.isDouble() || !x2.isDouble() || !y2.isDouble())
+        if (x1.isDouble() && y1.isDouble() && x2.isDouble() && y2.isDouble())
         {
             Line line(
                     Point(x1.asDouble(), y1.asDouble()),
@@ -106,6 +121,7 @@ struct Line
             );
             return line;
         } else {
+            std::cout << "One of the points is not a float" << std::endl;
             return nonstd::nullopt;
         }
     }
@@ -159,7 +175,7 @@ int main()
                 resp->setBody("All good");
                 callback(resp);
             },
-            {Get}
+            {Post, Get}
     );
 
     drogon::app().registerHandler("/count",
@@ -176,12 +192,14 @@ int main()
     drogon::app().registerHandler("/status_update",
             [&count](const drogon::HttpRequestPtr& req,
                     std::function<void (const HttpResponsePtr &)> &&callback, const std::string&) {
+                std::cout << "status_update" << std::endl;
                 std::shared_ptr<Json::Value> json_ptr = req->jsonObject();
                 if (!json_ptr)
                 {
                     auto resp=HttpResponse::newHttpResponse();
-                    resp->setContentTypeCode(CT_APPLICATION_JSON);
+                    resp->setContentTypeCode(CT_TEXT_PLAIN);
                     resp->setStatusCode(k400BadRequest);
+                    resp->setBody("Invalid json");
                     callback(resp);
                 } else {
 
@@ -191,8 +209,9 @@ int main()
                     if (!delta.isInt())
                     {
                         auto resp=HttpResponse::newHttpResponse();
-                        resp->setContentTypeCode(CT_APPLICATION_JSON);
+                        resp->setContentTypeCode(CT_TEXT_PLAIN);
                         resp->setStatusCode(k400BadRequest);
+                        resp->setBody("Must have an int in 'delta'");
                         callback(resp);
                     } else {
                         int change = delta.asInt();
@@ -204,7 +223,7 @@ int main()
                     }
                 }
             },
-            {Get}
+            {Post}
     );
 
     drogon::app().registerHandler("/register",
@@ -213,15 +232,18 @@ int main()
                 std::shared_ptr<Json::Value> data = req->jsonObject();
                 if (!data) {
                     auto resp=HttpResponse::newHttpResponse();
-                    resp->setContentTypeCode(CT_APPLICATION_JSON);
+                    resp->setContentTypeCode(CT_TEXT_PLAIN);
                     resp->setStatusCode(k400BadRequest);
+                    resp->setBody("Invalid json");
                     callback(resp);
                 } else {
-                    nonstd::optional<Device> device = Device::from_json(*data);
+                    trantor::InetAddress ip = req->getPeerAddr();
+                    nonstd::optional<Device> device = Device::from_json(*data, ip);
                     if (!device) {
                         auto resp=HttpResponse::newHttpResponse();
-                        resp->setContentTypeCode(CT_APPLICATION_JSON);
+                        resp->setContentTypeCode(CT_TEXT_PLAIN);
                         resp->setStatusCode(k400BadRequest);
+                        resp->setBody("Device not in correct format");
                         callback(resp);
                     } else {
                         devices.push_back(*device);
@@ -276,15 +298,17 @@ int main()
                 if (!json_ptr)
                 {
                     auto resp=HttpResponse::newHttpResponse();
-                    resp->setContentTypeCode(CT_APPLICATION_JSON);
+                    resp->setContentTypeCode(CT_TEXT_PLAIN);
                     resp->setStatusCode(k400BadRequest);
+                    resp->setBody("Invalid JSON");
+
                     callback(resp);
                 } else {
                     const Json::Value& json = *json_ptr;
                     nonstd::optional<Line> in = Line::from_json(json["line_in"]);
                     nonstd::optional<Line> out = Line::from_json(json["line_out"]);
 
-                    if (!in || !out)
+                    if (in && out)
                     {
                         config.in = *in;
                         config.out = *out;
@@ -294,13 +318,14 @@ int main()
                         callback(resp);
                     } else {
                         auto resp=HttpResponse::newHttpResponse();
-                        resp->setContentTypeCode(CT_APPLICATION_JSON);
+                        resp->setContentTypeCode(CT_TEXT_PLAIN);
                         resp->setStatusCode(k400BadRequest);
+                        resp->setBody("In/out lines not specified correctly");
                         callback(resp);
                     }
                 }
             },
-            {Get}
+            {Post}
     );
 
 
