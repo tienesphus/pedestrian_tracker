8de5b99969174493fc20798b38f2d59fdd97f517
Nick <nanzellotti@live.com>
2021-09-29 02:38:05 +1000

    Added further comments

--- pedestrian_tracker/cpp/include/logObject.hpp
+++ pedestrian_tracker/cpp/include/logObject.hpp
@@ -21,18 +21,18 @@ class LogInformation
     //New Demo Location 3 (Location)
 
     private:
-    std::string segment;        ///< 
+    std::string segment;        ///< used to identify each log value
 
     public:
     std::string frameNumber;    ///< the frame number
     std::string dateTime;       ///< time of the of the logging
-    std::string peopleInFrame;  ///<
+    std::string peopleInFrame;  ///< amount of people in the frame
     std::string location;       ///< location of the system
-    int x_Location;             ///< 
-    int y_Location;             ///< 
-    int x_box;                  ///<
-    int y_box;                  ///<
-    int confidence;             ///<
+    int x_Location;             ///< the x chord top left corner of the user identified
+    int y_Location;             ///< the y chord top left corner of the user identified
+    int x_box;                  ///< the width of the box drawn around the person for the given frame
+    int y_box;                  ///< the height of the box drawn around the person for the given frame
+    int confidence;             ///< the confidence interval of the identification for the given frame
     int uniqueID;               ///< pedestrain unique id
     //Split Log into relivant fields
     LogInformation(std::stringstream &log);
